1ceabd62f858f5b4c555fd90c1f84d82c6254e9d
Emil Ramazanov <emil.ramazanovvv@gmail.com>
2025-12-04 23:29:43 +0300

    rectangle test added

---
+++ test_rectangle.py
@@ -0,0 +1,55 @@
+import unittest
+from rectangle import area, perimeter
+
+
+class RectangleTestCase(unittest.TestCase):
+    def test_area_normal(self):
+        """Тест площади прямоугольника с нормальными значениями"""
+        res = area(5, 10)
+        self.assertEqual(res, 50)
+
+    def test_area_zero_width(self):
+        """Тест площади прямоугольника с нулевой шириной"""
+        res = area(10, 0)
+        self.assertEqual(res, 0)
+
+    def test_area_zero_height(self):
+        """Тест площади прямоугольника с нулевой высотой"""
+        res = area(0, 10)
+        self.assertEqual(res, 0)
+
+    def test_area_square(self):
+        """Тест площади квадрата (прямоугольник с равными сторонами)"""
+        res = area(10, 10)
+        self.assertEqual(res, 100)
+
+    def test_area_float(self):
+        """Тест площади прямоугольника с дробными значениями"""
+        res = area(2.5, 4.0)
+        self.assertEqual(res, 10.0)
+
+    def test_perimeter_normal(self):
+        """Тест периметра прямоугольника с нормальными значениями"""
+        res = perimeter(5, 10)
+        self.assertEqual(res, 30)
+
+    def test_perimeter_zero_width(self):
+        """Тест периметра прямоугольника с нулевой шириной"""
+        res = perimeter(0, 10)
+        self.assertEqual(res, 20)
+
+    def test_perimeter_zero_height(self):
+        """Тест периметра прямоугольника с нулевой высотой"""
+        res = perimeter(10, 0)
+        self.assertEqual(res, 20)
+
+    def test_perimeter_square(self):
+        """Тест периметра квадрата"""
+        res = perimeter(10, 10)
+        self.assertEqual(res, 40)
+
+    def test_perimeter_float(self):
+        """Тест периметра прямоугольника с дробными значениями"""
+        res = perimeter(2.5, 4.0)
+        self.assertEqual(res, 13.0)
+
